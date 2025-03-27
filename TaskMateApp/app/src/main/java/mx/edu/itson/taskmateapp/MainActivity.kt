67268199bbda2cde54efd8268a6d75331b6145bd
abel11604 <abelcorona.quintero@gmail.com>
2025-03-26 22:31:47 -0700

    imageSlider
    borre lo de las fotos circulares por el momento por que daba error(Solo eran compatibles con constraintLayout y no era compatible con ImagesViews)

--- TaskMateApp/app/src/main/java/mx/edu/itson/taskmateapp/MainActivity.kt
+++ TaskMateApp/app/src/main/java/mx/edu/itson/taskmateapp/MainActivity.kt
@@ -3,12 +3,20 @@ package mx.edu.itson.taskmateapp
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import org.imaginativeworld.whynotimagecarousel.ImageCarousel
+import org.imaginativeworld.whynotimagecarousel.model.CarouselItem
 
 class MainActivity : AppCompatActivity() {
+    private val list = mutableListOf<CarouselItem>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        val carousel: ImageCarousel = findViewById(R.id.carousel)
+        list.add(CarouselItem(R.drawable.panda1))
+        list.add(CarouselItem(R.drawable.panda2))
+        list.add(CarouselItem(R.drawable.panda3))
 
+        carousel.addData(list)
     }
 }
