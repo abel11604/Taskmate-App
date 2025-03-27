f892c4e441c6899cd63ad151a6e62542c2af2492
abel11604 <abelcorona.quintero@gmail.com>
2025-03-27 12:58:15 -0700

    falta navegacion

--- TaskMateApp/app/src/main/java/mx/edu/itson/taskmateapp/members.kt
+++ TaskMateApp/app/src/main/java/mx/edu/itson/taskmateapp/members.kt
@@ -10,11 +10,5 @@ class members : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_members)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
     }
 }
