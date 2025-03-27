baaf308b8a21863211788dd3aeaccad711da917f
AdolfoVenegas <adolfo.restaurantly@gmail.com>
2025-03-27 01:29:29 -0700

    members

---
+++ TaskMateApp/app/src/main/java/mx/edu/itson/taskmateapp/member_configuration.kt
@@ -0,0 +1,20 @@
+package mx.edu.itson.taskmateapp
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class member_configuration : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_member_configuration)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
