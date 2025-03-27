341f6e314b9cf521231318dc7f83e55f9129aa8f
abel11604 <abelcorona.quintero@gmail.com>
2025-03-27 11:32:28 -0700

    ya jala el navbar :)

--- TaskMateApp/app/src/main/java/mx/edu/itson/taskmateapp/ConifgActivity.kt
+++ TaskMateApp/app/src/main/java/mx/edu/itson/taskmateapp/ConifgActivity.kt
@@ -10,7 +10,7 @@ class ConifgActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_conifg)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
--- TaskMateApp/app/src/main/java/mx/edu/itson/taskmateapp/RegistroActivity.kt
+++ TaskMateApp/app/src/main/java/mx/edu/itson/taskmateapp/RegistroActivity.kt
@@ -11,10 +11,6 @@ class RegistroActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_registro)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
     }
 }
